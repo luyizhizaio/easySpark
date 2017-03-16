eb0aef9735d12655de143fae10a3b9b347a0d224
lichangyue <lichangyue528@sina.com>
2017-03-16 15:12:43 +0800

    特征工程

--- src/main/scala/com/mllib/feature/VectorIndexerTest.scala
+++ src/main/scala/com/mllib/feature/VectorIndexerTest.scala
@@ -1,6 +1,7 @@
 package com.mllib.feature
 
 import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 
@@ -19,12 +20,22 @@ object VectorIndexerTest {
 
 
 
-    val data = sqlContext.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+    //val data = sqlContext.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
+    val data = sqlContext.createDataFrame(Seq(
+      (1,Vectors.dense(1,0.2,2)),
+      (0,Vectors.dense(2,0.2,22)),
+      (0,Vectors.dense(1,0.4,21)),
+      (1,Vectors.dense(1,0.5,52)),
+      (1,Vectors.dense(1,0.3,32)),
+      (1,Vectors.dense(1,0.7,1))
+    )).toDF("label","features")
+
 
     val indexer = new VectorIndexer()
     .setInputCol("features")
     .setOutputCol("indexed")
-    .setMaxCategories(10)
+    .setMaxCategories(4) //超过4就当做连续型数值
 
     val indexerModel = indexer.fit(data)
 
@@ -36,6 +47,17 @@ object VectorIndexerTest {
     val indexedData = indexerModel.transform(data)
 
     indexedData.show()
+
+//    +-----+--------------+--------------+
+//    |label|      features|       indexed|
+//    +-----+--------------+--------------+
+//    |    1| [1.0,0.2,2.0]| [0.0,0.2,2.0]|
+//    |    0|[2.0,0.2,22.0]|[1.0,0.2,22.0]|
+//    |    0|[1.0,0.4,21.0]|[0.0,0.4,21.0]|
+//    |    1|[1.0,0.5,52.0]|[0.0,0.5,52.0]|
+//    |    1|[1.0,0.3,32.0]|[0.0,0.3,32.0]|
+//    |    1| [1.0,0.7,1.0]| [0.0,0.7,1.0]|
+//    +-----+--------------+--------------+
 
   }
 
