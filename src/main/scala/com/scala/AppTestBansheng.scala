39c1ce6600eed8fbb90c92000003ef63aa253c81
luyizhizaio <lichangyue528@sina.com>
2018-06-30 22:18:44 +0800

    scala  Option 测试

---
+++ src/main/scala/com/scala/AppTestBansheng.scala
@@ -0,0 +1,29 @@
+package com.scala
+
+/**
+ * Created by dayue on 2017/5/8.
+ */
+class AppTestBansheng private {
+
+
+  private val key="something.key"
+
+
+  private def update():Unit = {
+    println("update method")
+  }
+
+
+
+}
+
+object AppTestBansheng{
+
+  private val key2 = "key2"
+
+//  def  insert
+  def apply():AppTestBansheng={
+    new AppTestBansheng()
+}
+
+}
